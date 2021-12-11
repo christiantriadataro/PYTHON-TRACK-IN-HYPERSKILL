caa773eb509dfd7d80638307c3dcf8f87cfd5a5e
chanchanchong <ultrainstinct2030@gmail.com>
2021-12-11 18:16:37 +0800

    update

--- Computer science/Programming languages/Python/Object-oriented programming/Methods/Magic methods/Patients/exercise.py
+++ Computer science/Programming languages/Python/Object-oriented programming/Methods/Magic methods/Patients/exercise.py
@@ -7,4 +7,27 @@
 # - For users: {name} {last_name}.{age}
 
 # For example, for object john = Patient("John", "Doe", 50)
-# these representations would repe
+# these representations would respectively look like this:
+# - Object of the class Patent.name: John, last_name: Doe,
+#   age: 50
+# - John Doe.50
+
+# Create the necessary method below. Pay attention to spaces
+# and punctuation in the strings.
+
+class Patient:
+    def __init__(self, name, last_name, age):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+
+    # create methods here
+    def __repr__(self):
+        return f"Object of the class Patient. name: {self.name}, last_name: {self.last_name}, age: {self.age}"
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}. {self.age}"
+
+john = Patient("John", "Doe", 50)
+print(john.__repr__())
+print(john)
