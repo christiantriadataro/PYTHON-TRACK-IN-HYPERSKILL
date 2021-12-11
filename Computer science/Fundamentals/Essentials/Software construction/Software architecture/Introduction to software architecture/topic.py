8ff38ccc1401d525aa596030dda2c778e78d7654
Christian Dataro <58933979+chanchanchong@users.noreply.github.com>
2021-12-12 02:37:56 +0800

    update

---
+++ Computer science/Fundamentals/Essentials/Software construction/Software architecture/Introduction to software architecture/topic.py
@@ -0,0 +1,27 @@
+# Theory: Introduction to software architecture
+
+# At the beginning of a development cycle for a complex
+# application that combines multiples components, you may
+# notice that you need to have a proper structure that
+# represents all interactions happening within. Software
+# architecture is the term that defines said structure. IT
+# helps understand the relationships between each major
+# system component and simultaneously provides the
+# necessary documentation for developers and clients.
+
+# Every project that combines multiple different
+# components has to have a clear software architecture to
+# define technical and structural requirements for the
+# system. It allows reducing any possible risk when
+# creating a project. If you need to complete your project
+# and establish communications within a team in a short
+# amount of time, you need to master software
+# architecture.
+
+# 1. What is software architecture?
+# For example, you want to develop a brand new
+# application, that potentially could do whatever you want.
+# What will be the first thing that you'll do? Your answer
+# should be, "Divide my application into several
+# components, write documentation and draw a scheme of
+# all the connections within it".
